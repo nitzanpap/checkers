ce8a09b809804aaa2a41121f9c5a0c4394ff8cfc
nitzanpap <nitzanpap@gmail.com>
2022-05-03 17:54:41 +0300

    Added documentation to the Piece class

--- js/classes/Piece.js
+++ js/classes/Piece.js
@@ -1,4 +1,14 @@
+/**
+ * A class representing a single piece
+ */
 class Piece {
+    /**
+     * Creates a single piece
+     * @param {Number} row Row index
+     * @param {Number} col Column index
+     * @param {String} type The type of the piece
+     * @param {String} color The color of the piece
+     */
     constructor(row, col, type, color) {
         this.row = row
         this.col = col
@@ -9,17 +19,30 @@ class Piece {
         this.threatened = false
     }
 
+    /**
+     * Sets the row and coordinate information of the piece
+     * @param {Number} row Row index
+     * @param {Number} col Column index
+     */
     setRowAndColumn(row, col) {
         this.row = row
         this.col = col
     }
 
+    /**
+     * Raises the threatened flag attribute
+     * @param {Piece} piece A given piece
+     */
     threatenThisPiece(piece) {
         piece.threatened = true
     }
 
+    /**
+     * Gets all of the possible moves this piece can make
+     * @param {object} board the array containg all of the pieces
+     * @returns {object} An array containing all the possible moves of this piece
+     */
     getPossibleMoves(board) {
-        // Get relative moves
         let absoluteMoves = []
         if (this.type === ILLEGAL || this.type === EMPTY) {
             absoluteMoves = []
