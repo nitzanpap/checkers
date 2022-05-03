e5ddc8df64466a45aed547db2dc2a7b7bb791a0b
nitzanpap <nitzanpap@gmail.com>
2022-05-03 15:42:03 +0300

    Fixed switchTurn()

--- js/utils.js
+++ js/utils.js
@@ -7,7 +7,7 @@ function coordinateToCheckersCoordinate(row, col) {
     return String.fromCharCode(asciiNumOfA + row) + (col + 1)
 }
 
-function switchTurn(currentPlayerTurn) {
+function switchTurn() {
     currentPlayerTurn = currentPlayerTurn === WHITE_PLAYER ? BLACK_PLAYER : WHITE_PLAYER
 }
 
