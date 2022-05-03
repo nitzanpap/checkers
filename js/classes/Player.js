debfa7617933f9a4e03f918adf564c0a55dfc3dc
nitzanpap <nitzanpap@gmail.com>
2022-05-03 17:41:20 +0300

    Added Documentation to the Player class

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,8 +1,20 @@
+/**
+ * A class representing a player (i.e. White/Black player)
+ */
 class Player {
+    /**
+     * Creates a player
+     * @param {*} color The color of the player
+     */
     constructor(color) {
         this.color = color
         this.piecesLeft = 12
     }
+
+    /**
+     * Reduces the players amount of pieces left on the board by 1.
+     * @param {Piece} piece a piece from the board array
+     */
     pieceCaptured(piece) {
         this.piecesLeft--
     }
