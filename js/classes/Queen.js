0dd41f7f40b6ed3031306beb0a919a89579d0f87
nitzanpap <nitzanpap@gmail.com>
2022-05-03 18:11:27 +0300

    Added documentation to classes queen and soldier

--- js/classes/Queen.js
+++ js/classes/Queen.js
@@ -1,8 +1,21 @@
+/**
+ * A class representing a single Queen piece
+ */
 class Queen extends Piece {
+    /**
+     * Creates a single Queen piece
+     * @param {Number} row Row index
+     * @param {Number} col Column index
+     * @param {String} color The color of the piece
+     */
     constructor(row, col, color) {
         super(row, col, QUEEN, color)
     }
-
+    /**
+     * Gets all of the possible moves this piece can make
+     * @param {object} board the array containg all of the pieces
+     * @returns {object} An array containing all the possible moves of this piece
+     */
     getPossibleMoves(board) {
         let absoluteMoves = []
         let capturingMoves = []
@@ -11,6 +24,7 @@ class Queen extends Piece {
         } else {
             console.log("Unknown type", type)
         }
+        //
         for (let absoluteMove of absoluteMoves) {
             if (absoluteMove[2] === CAPTURE) {
                 capturingMoves.push(absoluteMove)
@@ -19,14 +33,21 @@ class Queen extends Piece {
         return capturingMoves.length !== 0 ? capturingMoves : absoluteMoves
     }
 
-    getMovesInDirection(result, i, j, rowDirection, colDirection, encounteredOpponent) {
+    /**
+     *
+     * @param {object} result An array containing all the possible moves of this piece
+     * @param {Number} i Row index
+     * @param {Number} j Column index
+     * @param {Number} rowDirection An incremanter for the row index
+     * @param {Number} colDirection An incremanter for the column index
+     */
+    getMovesInDirection(result, i, j, rowDirection, colDirection) {
         i += rowDirection
         j += colDirection
         // If cell is in bounds
         if (isCoordinateInBounds(i, j)) {
             // If encountered an opponent piece
             if (board[i][j].color === this.opponentColor) {
-                encounteredOpponent = true
                 if (
                     isCoordinateInBounds(i + rowDirection, j + colDirection) &&
                     board[i + rowDirection][j + colDirection].type === EMPTY
@@ -37,26 +58,23 @@ class Queen extends Piece {
                 }
             }
             // If encountered an empty tile
-            else if (board[i][j].color === "e" && !encounteredOpponent) {
+            else if (board[i][j].color === "e") {
                 result.push([i, j, MOVE])
-                this.getMovesInDirection(
-                    result,
-                    i,
-                    j,
-                    rowDirection,
-                    colDirection,
-                    encounteredOpponent
-                )
+                this.getMovesInDirection(result, i, j, rowDirection, colDirection)
             }
         }
     }
 
+    /**
+     * Gets all the possible moves that this Queen can make
+     * @returns {object} An array containing all the possible moves of this piece
+     */
     getQueenRelativeMoves() {
         let result = []
-        this.getMovesInDirection(result, this.row, this.col, 1, 1, false)
-        this.getMovesInDirection(result, this.row, this.col, -1, -1, false)
-        this.getMovesInDirection(result, this.row, this.col, 1, -1, false)
-        this.getMovesInDirection(result, this.row, this.col, -1, 1, false)
+        this.getMovesInDirection(result, this.row, this.col, 1, 1)
+        this.getMovesInDirection(result, this.row, this.col, -1, -1)
+        this.getMovesInDirection(result, this.row, this.col, 1, -1)
+        this.getMovesInDirection(result, this.row, this.col, -1, 1)
         return result
     }
 }
--- js/classes/Soldier.js
+++ js/classes/Soldier.js
@@ -1,9 +1,23 @@
+/**
+ * A class representing a single Soldier piece
+ */
 class Soldier extends Piece {
+    /**
+     * Creates a single Soldier piece
+     * @param {Number} row Row index
+     * @param {Number} col Column index
+     * @param {String} color The color of the piece
+     */
     constructor(row, col, color) {
         super(row, col, SOLDIER, color)
         this.lastRow = this.color === WHITE ? BOARD_SIZE - 1 : 0
     }
 
+    /**
+     * Gets all of the possible moves this piece can make
+     * @param {object} board the array containg all of the pieces
+     * @returns {object} An array containing all the possible moves of this piece
+     */
     getPossibleMoves(board) {
         let absoluteMoves = []
         let capturingMoves = []
@@ -20,6 +34,14 @@ class Soldier extends Piece {
         return capturingMoves.length !== 0 ? capturingMoves : absoluteMoves
     }
 
+    /**
+     *
+     * @param {object} result An array containing all the possible moves of this piece
+     * @param {Number} i Row index
+     * @param {Number} j Column index
+     * @param {Number} rowDirection An incremanter for the row index
+     * @param {Number} colDirection An incremanter for the column index
+     */
     getMovesInDirection(result, i, j, rowDirection, colDirection) {
         i += rowDirection
         j += colDirection
@@ -45,6 +67,10 @@ class Soldier extends Piece {
         }
     }
 
+    /**
+     * Gets all the possible moves that this Soldier can make
+     * @returns {object} An array containing all the possible moves of this piece
+     */
     getSoldierRelativeMoves() {
         let result = []
         if (this.color === WHITE) {
